9db4b0b12c45c576ef930918f26d09a1e507fbb0
Chaimae.bk <sha663277@gmail.com>
2025-08-30 13:29:54 +0100

    Add files via upload

---
+++ library 1.py
@@ -0,0 +1,37 @@
+books = [ ]
+book = input('Enter the name of a book you own: ').lower()
+books.append(book)
+book = input("Enter another name of a book you own(or press 'Enter' to skip: ").lower()
+if book :
+	books.append(book)
+	print(f'your library: {books}')
+else :
+	print(f'your library: {books}')
+
+wishlist = [ ]
+wish = input('\nEnter the name of a book you wish to have in future: ').lower()
+wishlist.append(wish)
+wish = input("Enter another name of book you wish to have (or press 'Enter' to skip): ").lower()
+if wish:
+	wishlist.append(wish)
+	print(f'your wishlist: {wishlist}')
+else:
+	print(f'your wishlist: {wishlist}')
+	
+got = input("\nEnter the name of a book from your wishlist that you've acquired (or press 'Enter' to skip): ").lower()
+if got :
+	
+	books.append(got)
+	wishlist.remove(got)
+	print(f'Updated library : {books}')
+	print(f'Updated wishlist : {wishlist}')
+else:
+	print(f'Updated library : {books}')
+	print(f'Updated wishlist : {wishlist}')
+
+don = input("\nEnter the name of a book from your library you wish to donate (or press 'Enter' to skip): ").lower()
+if don:
+	books.remove(don)
+	print(f'final library after donation: {books}')
+else :
+	print(f'final library after donation: {books}')
